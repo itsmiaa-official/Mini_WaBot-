898fad81b2e64253c590cb1eef45845aadc36124
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-09-28 10:12:24 -0300

    Update group-bot.js

--- plugins/group-bot.js
+++ plugins/group-bot.js
@@ -3,9 +3,9 @@ let handler = async (m, { conn, text, args }) => {
   if (!args[0]) {
     const estado = global.db.data.chats[m.chat]?.bannedGrupo ?? false;
     const estadoTexto = estado ? '✗ Desactivado' : '✓ Activado';
-    const info = `*🫟 Estado del Bot (｡•́‿•̀｡)*\n` +
-                 `🫗 *Actual ›* ${estadoTexto}\n\n` +
-                 `🌱 Puedes cambiarlo con:\n` +
+    const info = `*🇨🇳 Estado del Bot (｡•́‿•̀｡)*\n` +
+                 `🔥 *Actual ›* ${estadoTexto}\n\n` +
+                 `💋 Puedes cambiarlo con:\n` +
                  `> ● _Activar ›_ *bot on*\n` +
                  `> ● _Desactivar ›_ *bot off*`;
     return m.reply(info);
@@ -17,20 +17,20 @@ let handler = async (m, { conn, text, args }) => {
     const accion = args[0].toLowerCase();
 
     if (accion === 'off') {
-      if (estado) return m.reply('🌱 El *Bot* ya estaba *desactivado* en este grupo.');
+      if (estado) return m.reply('🔥La *Bot* ya estaba *desactivado* en este grupo.');
       chat.bannedGrupo = true;
-      return m.reply('🫟 Has *Desactivado* el *Bot* en este grupo.');
+      return m.reply('💋 Has *Desactivado* la *Bot* en este grupo.');
     }
 
     if (accion === 'on') {
-      if (!estado) return m.reply('🌱 El *Bot* ya estaba *activado* en este grupo.');
+      if (!estado) return m.reply('🔥 La *Bot* ya estaba *activado* en este grupo.');
       chat.bannedGrupo = false;
-      return m.reply('🫟 Has *Activado* el *Bot* en este grupo.');
+      return m.reply('💋 Has *Activado* la *Bot* en este grupo.');
     }
 
-    return m.reply('🌿 Usa: *bot on* o *bot off* para cambiar el estado.');
+    return m.reply('🇨🇳 Usa: *bot on* o *bot off* para cambiar el estado.');
   } catch (e) {
-    await m.reply('🕸 Ocurrió un error al intentar cambiar el estado del bot.');
+    await m.reply('🔥 Ocurrió un error al intentar cambiar el estado de la bot.');
   }
 };
 
