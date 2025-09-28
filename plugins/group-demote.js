1162395d8dc9ad7f45608818be6c3383bf15e822
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-09-28 10:16:28 -0300

    Update group-demote.js

--- plugins/group-demote.js
+++ plugins/group-demote.js
@@ -3,25 +3,25 @@ const handler = async (m, { conn }) => {
     // let who = m?.message?.extendedTextMessage?.contextInfo?.participant || m?.mentionedJid[0] || await m?.quoted?.sender;
    let texto = await m.mentionedJid
    let who = texto.length > 0 ? texto[0] : (m.quoted ? await m.quoted.sender : false)
-    if (!who) return m.reply('🌾 Menciona al usuario que deseas degradar de administrador.');
+    if (!who) return m.reply('⚠️ Menciona al usuario que deseas degradar de administrador.');
 
     const groupMetadata = await conn.groupMetadata(m.chat);
     const participant = groupMetadata.participants.find(participant => participant.jid === who);
 
     if (!participant || !participant.admin) {
-    return conn.reply(m.chat, `🌾 *@${who.split('@')[0]}* no es administrador del grupo!`, m, { mentions: [who] });
+    return conn.reply(m.chat, `❌ *@${who.split('@')[0]}* no es administrador del grupo!`, m, { mentions: [who] });
     }
 
     if (who === groupMetadata.owner) {
-      return m.reply('⭐ No puedes degradar al creador del grupo de administrador.');
+      return m.reply('🔥 No puedes degradar al creador del grupo de administrador.');
     }
 
     if (who === conn.user.jid) {
-      return m.reply('⭐ No puedes degradar al bot de administrador.');
+      return m.reply('🔥 No puedes degradar al bot de administrador.');
     }
 
     await conn.groupParticipantsUpdate(m.chat, [who], 'demote');
-    await conn.reply(m.chat, `🕸 *@${who.split('@')[0]}* ha sido degradado de administrador del grupo!`, m, { mentions: [who] });
+    await conn.reply(m.chat, `💋 *@${who.split('@')[0]}* ha sido degradado de administrador del grupo!`, m, { mentions: [who] });
   } catch (e) {
     await m.reply(`🐼 Error.`);
   }
