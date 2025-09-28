573a992eb7513a76609e4822e1911deb4ade6f15
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-09-27 21:54:27 -0300

    Update jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -198,7 +198,7 @@ let chtxt = ` ֯　ׅ🫗ֶ֟ㅤ *Usuario ›* ${userName}
 > *¡Conviértete en un sub-bot ahora, únete al canal oficial!*\nhttps://whatsapp.com/channel/0029Vb6GYInD8SDuyzHk3f3l`
 
 let ppch = await sock.profilePictureUrl(m.sender, 'image').catch(_ => "https://stellarwa.xyz/files/1757206448404.jpeg")
-await global.conn.sendMessage(my.ch2, { text: chtxt,
+await global.conn.sendMessage(my.ch1, { text: chtxt,
 contextInfo: { 
 externalAdReply: {
 title: "🔥 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗖𝗜𝗢́𝗡 𝗚𝗘𝗡𝗘𝗥𝗔𝗟 🔥",
