de3d578927729cd700a37bf7d59ff47f9cc0f6f3
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-09-28 10:20:32 -0300

    Update group-config.js

--- plugins/group-config.js
+++ plugins/group-config.js
@@ -3,36 +3,36 @@ let handler = async (m, { conn, command }) => {
     let groupMetadata = await conn.groupMetadata(m.chat);
     let groupAnnouncement = groupMetadata.announce;
 
-    if (command === 'closet') {
+    if (command === 'close') {
       if (groupAnnouncement === true) {
-        return conn.reply(m.chat, `🕸 El grupo ya está cerrado.`, m);
+        return conn.reply(m.chat, `⚠️ El grupo ya está cerrado.`, m);
       }
       await conn.groupSettingUpdate(m.chat, 'announcement')
         .then(() => {
-          conn.reply(m.chat, `🕸 El grupo ha sido cerrado correctamente.`, m);
+          conn.reply(m.chat, `🔥 El grupo ha sido cerrado correctamente.`, m);
         })
-        .catch((err) => conn.reply(m.chat, `🐼 Error al cerrar el grupo: ${err.message}`, m));
+        .catch((err) => conn.reply(m.chat, `❌ Error al cerrar el grupo: ${err.message}`, m));
     } else if (command === 'open') {
       if (groupAnnouncement === false) {
-        return conn.reply(m.chat, `🕸 El grupo ya está abierto.`, m);
+        return conn.reply(m.chat, `⚠️ El grupo ya está abierto.`, m);
       }
       await conn.groupSettingUpdate(m.chat, 'not_announcement')
         .then(() => {
-          conn.reply(m.chat, `🕸 El grupo ha sido abierto correctamente.`, m);
+          conn.reply(m.chat, `🔥 El grupo ha sido abierto correctamente.`, m);
         })
-        .catch((err) => conn.reply(m.chat, `🐼 Error al abrir el grupo: ${err.message}`, m));
+        .catch((err) => conn.reply(m.chat, `❌ Error al abrir el grupo: ${err.message}`, m));
     } else {
-      return conn.reply(m.chat, `🐼 *Error, reportelo al grupo de Soporte.*`, m);
+      return conn.reply(m.chat, `❌ *Error, reportelo al grupo de Soporte.*`, m);
     }
   } catch (e) {
     console.error(e);
-    conn.reply(m.chat, `⭐ *Error al realizar la configuración del grupo:* ${e.message}.`, m);
+    conn.reply(m.chat, `❌⚠️ *Error al realizar la configuración del grupo:* ${e.message}.`, m);
   }
 };
 
 handler.help = ['closet', 'open'];
 handler.tags = ['group'];
-handler.command = ['closet', 'open'];
+handler.command = ['close', 'open'];
 handler.admin = true;
 handler.botAdmin = true;
 
