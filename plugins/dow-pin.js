9a3f37a459c7dde8dcc13e33c9bb9a62607fae21
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-09-28 02:08:08 -0300

    Update dow-pin.js

--- plugins/dow-pin.js
+++ plugins/dow-pin.js
@@ -121,7 +121,7 @@ let handler = async (m, { conn, text }) => {
 
 handler.help = ['pinterest'];
 handler.command = ['pinterest', 'pin'];
-handler.tags = ['buscador'];
+handler.tags = ['dow'];
 
 
 export default handler;
