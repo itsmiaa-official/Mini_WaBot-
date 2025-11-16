fa44b801f92c00980a0a8f3ab3dbfa93946f499b
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-11-16 13:34:09 -0300

    Create info-menuutils.js

---
+++ plugins/info-menuutils.js
@@ -0,0 +1,76 @@
+import fs from 'fs';
+
+const filePath = './lib/personalize.json';
+
+let handler = async (m, { conn }) => {
+    try {
+        const data = JSON.parse(fs.readFileSync(filePath));
+
+        const globalConfig = data.global;
+        const defaultConfig = data.default;
+
+        const botName = globalConfig.botName || defaultConfig.botName;
+        const currency = globalConfig.currency || defaultConfig.currency;
+        const imagenes = globalConfig.imagenes.length > 0 ? globalConfig.imagenes : defaultConfig.imagenes;
+
+        const randomimagenesUrl = imagenes[Math.floor(Math.random() * imagenes.length)];
+
+        const menuMessage = `
+├┈ ↷ \`${botName}.\`
+├┈• ✐; ₊˚✦୧︰ \`${vs}.\`
+├┈┈・──・──・﹕₊˚ ✦・୨୧・        
+
+⭐ ¡𝖧𝗈𝗅𝖺! 𝖲𝗈𝗒 ${botName}, 𝖺𝗊𝗎𝗂 𝗍𝗂𝖾𝗇𝖾𝗌 𝗅𝖺 𝗅𝗂𝗌𝗍𝖺 𝖽𝖾 𝖼𝗈𝗆𝖺𝗇𝖽𝗈𝗌 🌸
+\`💰 𝖬𝗈𝗇𝖾𝖽𝖺 𝖺𝖼𝗍𝗎𝖺𝗅:\` ¥ ${currency}
+
+\`Canal Oficial:\`
+https://whatsapp.com/channel/0029Van1PcoFSAt50tWN4d0x
+
+\`Canal secundario:\`
+https://whatsapp.com/channel/0029Vb6GYInD8SDuyzHk3f3l
+
+
+˚ ₊ ‧  ꒰🛠꒱  — \`『 Utils 』\`
+✿ *#sticker › #s* 
+> _*Convierte imágenes o videos a stickers.*_
+✿ *#getpic › #pfp* + _<mention>_
+> _*Ver la foto de perfil de un usuario.*_
+✿ *#get* + _<url>_
+> _*Realiza solicitudes get a páginas web.*_
+✿ *#hd* 
+> _*Mejora la calidad de una imagen.*_
+✿ *#brat* + _<texto>_
+> _*Crea un sticker con texto.*_`;
+
+await conn.sendMessage(
+  m.chat,
+  {
+    text: menuMessage,
+    mentions: [m.sender],
+    contextInfo: {
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: my.ch,//"120363345778623279@newsletter",
+        serverMessageId: '',
+        newsletterName: my.name1 //"Canal de prueba"  
+      },
+      externalAdReply: {
+        title: `${botName}`,
+        body: `${copy} ${author}`,
+        thumbnailUrl: randomimagenesUrl,
+        mediaType: 1,
+        renderLargerThumbnail: true
+      }
+    }
+  }
+);
+    } catch (error) {
+        conn.reply(m.chat, `❌ Error al cargar el menú: ${error.message}`, m);
+    }
+};
+
+handler.help = ['menu'];
+handler.tags = ['info'];
+handler.command = ['menuutils'];
+
+export default handler;
