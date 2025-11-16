73a633edfbc0e80b6363035efb50453017ee9102
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-11-16 02:15:58 -0300

    Update owner-delbanner.js

--- plugins/owner-delbanner.js
+++ plugins/owner-delbanner.js
@@ -3,20 +3,20 @@ import fs from 'fs';
 const filePath = './lib/personalize.json';
 
 let handler = async (m, { text }) => {
-    if (!text) throw '❌ Debes proporcionar el enlace del video que deseas eliminar.';
+    if (!text) throw '❌ Debes proporcionar el enlace de la imagen que deseas eliminar.';
 
     const data = JSON.parse(fs.readFileSync(filePath));
 
-    if (!data.global || !data.global.videos || !data.global.videos.includes(text)) {
-        m.reply('❌ El video proporcionado no existe en la lista.');
+    if (!data.global || !data.global.imagenes || !data.global.imagenes.includes(text)) {
+        m.reply('❌ La imagen proporcionado no existe en la lista.');
         return;
     }
 
     // Eliminar el video de la lista global
-    data.global.videos = data.global.videos.filter((url) => url !== text);
+    data.global.imagenes = data.global.imagenes.filter((url) => url !== text);
 
     fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
-    m.reply('✅ Video eliminado correctamente.');
+    m.reply('✅ imagen eliminado correctamente.');
 };
 
 handler.help = ['delbanner <link del video>'];
