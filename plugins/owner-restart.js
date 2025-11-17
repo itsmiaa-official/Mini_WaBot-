b3e22c1b1165f897ed5320b28a2f794e36c99753
米娅·罗斯利亚科娃  🇨🇳 <itts.chinitaaa@gmail.com>
2025-11-17 01:54:18 -0300

    Update owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-  await conn.reply(m.chat, `🌾 Reiniciando el Socket...\n> 🐼 *Espere un momento...*`, m);
+  await conn.reply(m.chat, `⏰ Reiniciando a ${botName}...\n> 🌸 *Espere un momento...*`, m);
   setTimeout(() => {
     process.exit(0);
   }, 3000);
