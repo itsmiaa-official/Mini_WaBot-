cddbc45c6521e85b2db1bac2d054875885c5ccdb
𝐌𝐢𝐚 🍒 <hibqbqxdd@gmail.com>
2026-01-13 10:20:06 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,6 +43,9 @@ global.my = {
 
   ch2: '120363402839382986@newsletter', 
   name2: `=͟͟͞͞𝐒𝐩𝐚𝐜𝐞 𝐖𝐨𝐫𝐥𝐝 - 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 𝐔𝐩𝐝𝐚𝐭𝐞'𝐬 ✰`,
+
+ ch3: '120363407670600183@newsletter',
+ name3: '', 
 }
 
 const file = fileURLToPath(import.meta.url)
