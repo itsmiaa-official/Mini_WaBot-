1e70cd35803dd575c43cfccd8d863a04137c2643
𝐌𝐢𝐚 🍒 <hibqbqxdd@gmail.com>
2026-01-13 10:25:51 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,7 +23,7 @@ global.copy = '𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐛𝐲 𝕮𝖍𝖎𝖓𝖆'
 // ✼ •• ┈┈┈┈๑⋅⋯ ୨˚୧ ⋯⋅๑┈┈┈┈ •• ✼
 
 global.banner = 'https://files.catbox.moe/920ma6.jpg'
-global.icon = 'https://files.catbox.moe/469tnf.jpg'
+global.icon = 'https://files.catbox.moe/uqur0r.jpg'
 global.currency = 'Dólares 💸'
 global.sessions = 'sessions/session-bot'
 global.jadi = 'sessions/session-sub'
