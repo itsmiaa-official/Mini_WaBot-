3225f3d04d620243a6d149a5ff3fd7150a1e4d45
𝐌𝐢𝐚 🍒 <hibqbqxdd@gmail.com>
2026-01-13 10:38:06 -0300

    Update dow-tiktok.js

--- plugins/dow-tiktok.js
+++ plugins/dow-tiktok.js
@@ -28,15 +28,15 @@ const handler = async (m, { conn, text }) => {
     } = data
 
     const caption = `
-🇨🇳 *Título ›* ${title}
+🥷🏻 \`Título\` › ${title}
 
-👑 *Autor ›* ${author.nickname || author.unique_id || 'Desconocido'}
-> 🔥 *Duración ›* ${duration || 'N/A'}
-> 💋 *Likes ›* ${(stats.likes || 0).toLocaleString()}
-> 🔥 *Comentarios ›* ${(stats.comments || 0).toLocaleString()}
-> 💋 *Vistas ›* ${(stats.views || stats.plays || 0).toLocaleString()}
-> 🔥 *Compartidos ›* ${(stats.shares || 0).toLocaleString()}
-> 💋 *Audio ›* ${music.title ? music.title + " -" : 'Desconocido'} ${music.author ? music.author : ''}\n\n${dev}
+✰ \`Autor\` › ${author.nickname || author.unique_id || 'Desconocido'}
+> ✦ \`Duración\` › ${duration || 'N/A'}
+> ♡ \`Likes\` › ${(stats.likes || 0).toLocaleString()}
+> ☆ \`Comentarios\` › ${(stats.comments || 0).toLocaleString()}
+> ☘︎ \`Vistas\` › ${(stats.views || stats.plays || 0).toLocaleString()}
+> ➭ \`Compartidos\` › ${(stats.shares || 0).toLocaleString()}
+> • \`Audio\` › ${music.title ? music.title + " -" : 'Desconocido'} ${music.author ? music.author : ''}\n\n${dev}
     `.trim()
 
     const head = await fetch(dl, { method: 'HEAD' })
